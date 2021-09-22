e657aeaee8a3fb49b7c121f9293bf4576764a46e
Darshan hemanna <darshan.hemanna@oracle.com>
2021-09-22 21:22:21 +0530

    logging and exception added

--- and.py
+++ and.py
@@ -1,11 +1,18 @@
 from utils.model import perceptron
 from utils.all_utils import prepare_data, save_model,save_plot
 import pandas as pd
+import logging
+import os
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s] %(message)s"
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(filename= os.path.join(log_dir,"running_logs.log"),level=logging.INFO, format=logging_str, filemode="a")
 
 def main(data,ETA,epoch,filename,plot_filename):
 
     df = pd.DataFrame(data)
-    print(df)
+    logging.info(df)
 
     X,y = prepare_data(df)
 
@@ -26,6 +33,12 @@ if __name__ == "__main__": # entry point
 
     ETA = 0.3 # 0 and 1
     EPOCHS = 20
-    filename = 'and.model'
-    plot_filename = 'and.png'
-    main(AND,ETA,EPOCHS,filename,plot_filename)
+    try:
+        logging.info(">>>>> starting training >>>>>")
+        filename = 'and.model'
+        plot_filename = 'and.png'
+        main(AND,ETA,EPOCHS,filename,plot_filename)
+        logging.info("<<<<< training done successfully<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
